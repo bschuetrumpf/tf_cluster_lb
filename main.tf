a1eec91618500a71594872a3f6d0174717a8cb39
bschuetrumpf <72384760+bschuetrumpf@users.noreply.github.com>
2020-11-03 13:28:45 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,20 +11,18 @@ output "public_ip" {
   description  = "Public IP address of Web Server"
 }
 
-resource "aws_instance" "example" {
-  ami            = "ami-0c55b159cbfafe1f0"
-  instance_type  = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
   
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
-  tags = {
-    Name = "terraform-example-2"
+  min_size = 2
+  max_size = 10
+  
+  tag {
+    key                   = "Name"
+    value                 = "terraform_asg_example"
+    propagate_at_launch   = true
+    }
   }
-}
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
@@ -35,3 +33,20 @@ resource "aws_security_group" "instance" {
     cidr_blocks  = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_launch_configuration" "example" {
+  image_id       = "ami-0c55b159cbfafe1f0"
+  instance_type  = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
+  
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
