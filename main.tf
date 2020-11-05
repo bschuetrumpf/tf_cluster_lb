08818a5d61f89c3a633f11abca345eebf852be00
bschuetrumpf <72384760+bschuetrumpf@users.noreply.github.com>
2020-11-05 16:12:20 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-2"
 }
-
+#####DATA
 variable "server_port" {
   type = number
 }
@@ -13,14 +13,24 @@ data "aws_vpc" "default" {
 data "aws_subnet_ids" "default" {
   vpc_id = data.aws_vpc.default.id
 }
-
-resource "aws_lb" "example" {
-  name               = "terraform-asg-example"
-  load_balancer_type = "application"
-  subnets            = data.aws_subnet_ids.default.ids
+####SECURITY GROUPS
+resource "aws_security_group" {
+  name = "terraform-example-alb"
+  
+  ingress {
+    from_port     = 80
+    to_port       = 80
+    protocol      = "tcp"
+    cidr_blocks   = ["0.0.0.0/0"]
+    }
+  
+  egress {
+    from_port     = 0
+    to_port       = 0
+    protocol      = "-1"
+    cidr_blocks   = ["0.0.0.0/0"]
+    }
   }
-
-
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
@@ -32,9 +42,57 @@ resource "aws_security_group" "instance" {
   }
 }
 
+####LOAD BALANCER
+
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnet_ids.default.ids
+  security_groups    = [aws_security_group.alb.id]
+  }
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+  
+  default_action {
+    type = "fixed-response"
+    
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+      }
+    }
+  }
+
+resource "aws_lb_target_group" {
+  name        = "terraform-asg-example"
+  port        = var.server_port
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.default.id
+  
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher              = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    }
+  }
+
+
+
+####AUTO-SCALING GROUP
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+  
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
   
   min_size = 2
   max_size = 10
